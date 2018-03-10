2ec9a29339ce2e4b028e8db3670539f1c47c7f20
mj4worski <jaworskimateusz2@gmail.com>
2018-03-10 14:24:57 +0100

    Init commit with basic header.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1 className="header-primary">
+              <span className="header-primary--main">Interview questions</span>
+              <span className="header-primary--sub">Good luck</span>
+          </h1>
+        </header>
+        <div className="container">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
