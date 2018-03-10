8095e46f704c3ba036862cc37567239d9f76826d
mj4worski <jaworskimateusz2@gmail.com>
2018-03-10 14:43:16 +0100

    refactor: - change project structure. - extract Header to component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="header">
-          <h1 className="header-primary">
-              <span className="header-primary--main">Interview questions</span>
-              <span className="header-primary--sub">Good luck</span>
-          </h1>
-        </header>
+        <Header/>
         <div className="container">
 
         </div>
