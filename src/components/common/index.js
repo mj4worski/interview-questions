31944dc9c66284ae2b335a6577f32c088d2657d4
Mateusz Jaworski <mateusz.jaworski@grandparade.co.uk>
2018-07-11 19:26:53 +0200

    feat: provide button in QuestionProvider responsible for adding new question

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,2 +1,3 @@
 export { default as RadioButton } from './RadioButton';
 export { default as Button } from './Button';
+export { default as Box } from './Box';
--- src/components/questionsProvider/QuestionsProvider.jsx
+++ src/components/questionsProvider/QuestionsProvider.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import Box from '../common/Box';
+import { Box, Button } from '../common';
 import { getQuestions } from '../../api';
-import './QuestionsProvider.scss';
+import './QuestionsProvider.css';
+
+const AddQuestion = () => <Box>Bla bla bla</Box>;
 
 class QuestionsProvider extends Component {
+  state = {
+    addQuestionClicked: false
+  };
+
+  handleButtonClick = () => {
+    this.setState(prevState => {
+      return { addQuestionClicked: !prevState.addQuestionClicked };
+    });
+  };
+
   render() {
+    const { addQuestionClicked } = this.state;
     const questions = getQuestions();
     return (
       <Box>
-        <ul>{questions.map(({ question }) => <li>{question}</li>)}</ul>
+        <Box className="questions-provider-header">
+          Below you can see current questions in interview form. You have the
+          option of adding new question or current ones.
+          <Button onClick={this.handleButtonClick}>Add new question</Button>
+        </Box>
+        {addQuestionClicked && <AddQuestion />}
+        <ul className="questions-provider-list">
+          {questions.map(({ question }) => (
+            <li className="questions-provider-list">{question}</li>
+          ))}
+        </ul>
       </Box>
     );
   }
