653e3f0e6a5b61a1e5273a5ed314f519b8930052
Mateusz Jaworski <mateusz.jaworski@grandparade.co.uk>
2018-07-15 22:13:00 +0200

    feat: extension of AddQuestion with a new input for answer

--- src/components/questionsProvider/AddQuestion.jsx
+++ src/components/questionsProvider/AddQuestion.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Creatable } from 'react-select';
 import PropTypes from 'prop-types';
-import { Box, Button } from '../common';
+import { Box, Button, RadioButton } from '../common';
 import 'react-select/dist/react-select.css';
 import './AddQuestion.css';
 
@@ -32,26 +32,50 @@ export default class AddQuestion extends PureComponent {
 
     return (
       <Box className="add-question">
-        <label className="add-question__input-label">
-          New Question
-          <input
-            className="add-question__input"
-            type="text"
-            placeholder="Provide new question"
-          />
-        </label>
-        <label className="add-question__select-label">
-          Categories
-          <Creatable
-            options={categories.map(toValueLabelObject)}
-            onChange={this.handleOnChange}
-            value={category}
-            showNewOptionAtTop
-            className="add-question__select"
-            removeSelected
-          />
-        </label>
-        <Button className="add-question__button">Submit</Button>
+        <div className="layout-body">
+          <div className="layout-body__first-row">
+            <label className="add-question__label">
+              New Question
+              <input
+                className="add-question__input"
+                type="text"
+                placeholder="Provide new question"
+              />
+            </label>
+            <label className="add-question__label add-question__label--small">
+              Categories
+              <Creatable
+                options={categories.map(toValueLabelObject)}
+                onChange={this.handleOnChange}
+                value={category}
+                showNewOptionAtTop
+                className="add-question__select"
+                removeSelected
+              />
+            </label>
+          </div>
+          <div className="layout-body__second-row">
+            <label className="add-question__label">
+              Add answer
+              <input
+                className="add-question__input"
+                type="text"
+                placeholder="Provide answer"
+              />
+            </label>
+            <label className="add-question__label add-question__label--small">
+              Is correct answer ?
+              <RadioButton />
+            </label>
+            <Button className="add-question__button">Add answer</Button>
+          </div>
+        </div>
+
+        <div className="layout-footer">
+          If you do not want to add anything else click button next to the
+          message ;)
+          <Button className="add-question__button">Submit</Button>
+        </div>
       </Box>
     );
   }
